6afd99e83728d6d7494de0ef57ad8a332c362bb0
Eliazar Melendez <eliazar.melendez@gmail.com>
2014-09-04 19:15:18 -0600

    Nodo 2-3
    Se agrego el nodo 2-3

--- include/Node.h
+++ include/Node.h
@@ -16,6 +16,9 @@ class Node
         sf::Font font;
         sf::Text valueText;
 
+        int key[2];
+        Node *parent, *child[3];
+
         int value;
         Node* izquierdo;       // left child
         Node* derecho;      // right child
@@ -26,9 +29,19 @@ class Node
         void render( sf::RenderWindow* w);
         void setColor (sf::Color color);
         void setColorValue(sf::Color color); //Color del Texto
+
+        bool isLeaf();
+        int sibNumber();
+        void insert(Node *newChild, int newSmallest);
+
         virtual ~Node();
     protected:
     private:
+        int getSmallest();
+        void insert1Siblings(Node *newChild, int newSmallest);
+        void insert2Siblings(Node *newChild, int newSmallest);
+        void insert3Siblings(Node *newChild, int newSmallest);
+        void updateParentSmallest(int data);
 };
 
 #endif // NODE_H
--- src/Node.cpp
+++ src/Node.cpp
@@ -2,8 +2,7 @@
 #include  <iostream> ;
 #include <stdlib.h>
 float Node::radius = 50.0f;
-Node::Node(int data,float posX,float posY)
-{
+Node::Node(int data, float posX, float posY){
 
     circle.setFillColor(sf::Color::Black);
     circle.setRadius(radius);
@@ -14,6 +13,10 @@ Node::Node(int data,float posX,float posY)
     izquierdo = NULL;
     derecho = NULL;
     padre  = NULL;
+
+    key[0] = data;
+    key[1] = -1;
+    parent = child[0] = child[1] = child[2] = NULL;
 
 
     char chString[32];
@@ -28,25 +31,166 @@ Node::Node(int data,float posX,float posY)
 }
 
 
-Node::~Node()
-{
+Node::~Node(){
     //dtor
 }
 
- void Node::setColor (sf::Color color)
- {
-     circle.setFillColor(color);
- }
+void Node::setColor (sf::Color color){
+    circle.setFillColor(color);
+}
 
- void Node::setColorValue(sf::Color color)
-{
+void Node::setColorValue(sf::Color color){
     valueText.setColor(color);
 }
- void Node::render( sf::RenderWindow *w)
-  {
-    sf::RenderWindow *window = w;
 
-     window->draw(circle);
-     window->draw(valueText);
+void Node::render( sf::RenderWindow *w){
+sf::RenderWindow *window = w;
 
-  }
+ window->draw(circle);
+ window->draw(valueText);
+
+}
+
+bool Node::isLeaf(){
+    return (child[0] == NULL);
+}
+
+int Node::sibNumber(){
+    for (int i = 0; i < 3; ++i) {
+        if (this == parent->child[i]) return i;
+    }
+    return -1;
+}
+
+void Node::insert(Node *newChild, int newSmallest){
+    if (child[1] == NULL) insert1Siblings(newChild, newSmallest);
+    else if (child[2] == NULL) insert2Siblings(newChild, newSmallest);
+    else insert3Siblings(newChild, newSmallest);
+}
+
+int Node::getSmallest(){
+    Node *node = this;
+    while (!node->isLeaf()) node = node->child[0];
+    return node->key[0];
+}
+
+void Node::insert1Siblings(Node *newChild, int newSmallest){
+    int newKey = newChild->key[0];
+    newChild->parent = this;
+
+    if (newKey < child[0]->key[0]) {
+        // newNode is inserted as first child of root
+        child[1] = child[0];
+        child[0] = newChild;
+        key[0] = child[1]->getSmallest();
+    }else {
+        // newNode is iserted as second child of root
+        child[1] = newChild;
+        key[0] = newSmallest;
+    }
+}
+
+void Node::insert2Siblings(Node *newChild, int newSmallest){
+    int newKey = newChild->key[0];
+    newChild->parent = this;
+
+    if (newKey < child[0]->key[0]) {
+        child[2] = child[1];
+        child[1] = child[0];
+        child[0] = newChild;
+
+        key[1] = key[0];
+        key[0] = child[1]->getSmallest();
+        updateParentSmallest(newSmallest);
+    }
+    else if (newKey < child[1]->key[0]) {
+        child[2] = child[1];
+        child[1] = newChild;
+
+        key[1] = key[0];
+        key[0] = newSmallest;
+    }
+    else {
+        child[2] = newChild;
+
+        key[1] = newSmallest;
+    }
+}
+
+void Node::insert3Siblings(Node *newChild, int newSmallest){
+    int newKey = newChild->key[0];
+
+    int splitSmallest = -1;
+    Node *splitNode = new Node(-1, ((1024/2)-Node::radius), 50);
+    splitNode->parent = parent;
+
+    if (newKey < child[0]->key[0] || newKey < child[1]->key[0]) {
+        // newChild is inserted in current node
+        splitSmallest = key[0];
+        splitNode->child[0] = child[1];
+        splitNode->child[1] = child[2];
+        splitNode->key[0] = key[1];
+
+        child[1]->parent = splitNode;
+        child[2]->parent = splitNode;
+        newChild->parent = this;
+
+        if (newKey < child[0]->key[0]) {
+            // newChild is inserted as first child
+            child[1] = child[0];
+            child[0] = newChild;
+
+            key[0] = child[1]->getSmallest();
+            updateParentSmallest(newSmallest);
+        }
+        else {
+            // newChild is inserted as second child
+            child[1] = newChild;
+
+            key[0] = newSmallest;
+        }
+    }
+    else {
+        // newChild is inserted in split node
+        child[2]->parent = splitNode;
+        newChild->parent = splitNode;
+
+        if (newKey < child[2]->key[0]) {
+            // newChild is inserted as first child
+            splitSmallest = newSmallest;
+            splitNode->child[0] = newChild;
+            splitNode->child[1] = child[2];
+            splitNode->key[0] = key[1];
+        }
+        else {
+            // newChild is inserted as second child
+            splitSmallest = key[1];
+            splitNode->child[0] = child[2];
+            splitNode->child[1] = newChild;
+            splitNode->key[0] = newSmallest;
+        }
+    }
+
+    child[2] = NULL;
+    key[1] = -1;
+
+    if (parent->parent == NULL) {
+        // At root, so new root needs to be created
+        Node *newNode = new Node(-1, ((1024/2)-Node::radius), 50);
+
+        parent->child[0] = newNode;
+        newNode->parent = parent;
+        newNode->child[0] = this;
+        parent = newNode;
+    }
+
+    parent->insert(splitNode, splitSmallest);
+}
+
+void Node::updateParentSmallest(int data){
+    switch (sibNumber()){
+        case 0: if (parent->parent != NULL) parent->updateParentSmallest(data); break;
+        case 1: parent->key[0] = data; break;
+        case 2: parent->key[1] = data; break;
+    }
+}
